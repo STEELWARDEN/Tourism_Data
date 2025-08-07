f21e914a1d7978c5efe0c115a5e16ba79a51414e
Mohammed Halawani <xmb.bmx30@gmail.com>
2025-08-07 22:11:29 +0300

    Collected data using NASA api and added the functions needed to do that

--- Data_Api_Collection/data_fetch.py
+++ Data_Api_Collection/data_fetch.py
@@ -3,7 +3,11 @@ from dotenv import load_dotenv
 import pandas as pd
 import os
 
+
+
 load_dotenv()
+
+
 
 def get_data(date_str: str, page_no: int):
     url = os.getenv("API_KEY")
@@ -40,8 +44,70 @@ def get_data(date_str: str, page_no: int):
             return pd.DataFrame(), "unknown"
     else:
         return pd.DataFrame(), str(response.status_code)
+    
+
+
+def get_data_temp_single(latitude: float, longitude: float,time: int):
+        api_key = os.getenv("API_KEY_TEMP")
+        url = f"https://api.openweathermap.org/data/3.0/onecall/timemachine?lat={latitude}&lon={longitude}&dt={time}&units=metric&appid={api_key}"
+
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            if "data" in data:
+                temp = data["data"][0]["temp"]
+        else:
+            raise Exception(f"Error fetching temperature data: {response.status_code}") 
+        return temp, "200"
+
+def get_data_temp_bulk(latitude: float, longitude: float, start_time: int, end_time: int,province: str):
+    url = os.getenv("API_KEY_BULK")
+    params = {
+        "start": start_time,
+        "end": end_time,
+        "latitude": latitude,
+        "longitude": longitude,
+        "parameters": "T2M",  # Temperature at 2 meters
+        "community": "AG",
+        "format": "JSON"
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+    print(response)
+    print(data)
+
+    if "properties" not in data or "parameter" not in data["properties"]:
+        print(f"No temperature data for {province}")
+        return pd.DataFrame()
+
+    temp_data = data["properties"]["parameter"]["T2M"]
+
+    df = pd.DataFrame({
+        "date": temp_data.keys(),
+        "temp": temp_data.values(),
+        "province": province
+    })
+
+    df["year"] = df["date"].str[:4].astype(int)
+    df["month"] = df["date"].str[4:].astype(int)
+    df = df.drop(columns=["date"])
+
+    return df
+
+print(get_data_temp_bulk(24.7136, 46.6753, 2022, 2022, "Riyadh"))
+
+
+
+
+
+      
+
+        
+
+
+
 
     
-print(get_data("2024-01", 1))  # Example usage for testing
 
 
--- Data_Api_Collection/data_init.py
+++ Data_Api_Collection/data_init.py
@@ -1,11 +1,17 @@
 import data_fetch as gd
 import pandas as pd
+from datetime import datetime
+import time
 
 
 
 temp_data = "C:\\Users\\Mohammed\\Desktop\\Machine learning project for Tourism Data\\Data\\Temp_Province_data.csv"
 tourist_data = "C:\\Users\\Mohammed\\Desktop\\Machine learning project for Tourism Data\\Data\\Tourism_data.csv"
 
+
+
+
+ 
 
 
 def init_data_tourist():
@@ -41,4 +47,45 @@ def init_data_tourist():
           year += 1
 
 
+def init_temperature_data():
+     province_records = {"Jazan":(16.8894, 42.5706),"Riyadh":(24.7136, 46.6753),"Madinah":(24.4672, 39.6024),"Eastren":(23.5681, 50.6794),"Makkah":(21.4241, 39.8173),
+                            "Tabuk":(28.3835, 36.5662),"Hail":(27.5114, 41.7208),"Northern Borders":(30.0799, 42.8638),"Alqassim":(26.2078, 43.4837),
+                            "Albaha":(18.2465, 42.5117),"Najran":(17.5656, 44.2289),"Aseer":(19.0969, 42.8638),"Jouf":(29.8874, 39.3206),}
+     
+     column_names = ['month', 'year', 'province', 'temp']
+     df = pd.DataFrame(columns=column_names)
+     df.to_csv(temp_data, index=False)
+
+     
+     start_date_year = 2018
+     end_date_year = 2023
+    
+
+     for key in province_records:
+           latitude, longitude = province_records[key]
+           df_response = gd.get_data_temp_bulk(latitude,longitude,start_date_year,end_date_year,key)
+           if not df_response.empty:
+                    df_response = df_response[['month', 'year', 'province', 'temp']] 
+                    df_response.to_csv(temp_data, mode='a', header=False, index=False)
+           else:
+                raise Exception(f"Error fetching temperature data for {key}, {df_response}")
+
+init_temperature_data()
 init_data_tourist()
+
+
+
+
+
+          
+
+                    
+               
+                    
+                         
+                    
+                
+           
+     
+           
+
